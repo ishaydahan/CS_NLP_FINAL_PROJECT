f1e5afecbb130cfff560474aa051126216539920
Moshiko <moshiko8@gmail.com>
2017-07-24 12:49:37 +0300

    adding gui

---
+++ src/main/java/gui/CreateTest.java
@@ -0,0 +1,98 @@
+package gui;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.CardLayout;
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JToolBar;
+import javax.swing.JDesktopPane;
+import javax.swing.JTextPane;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Panel;
+
+public class CreateTest {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private int i;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void createTest() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CreateTest window = new CreateTest();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CreateTest() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 400, 150);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnGenerateTest = new JButton("Generate Test");
+		btnGenerateTest.setBounds(119, 77, 159, 23);
+		frame.getContentPane().add(btnGenerateTest);
+		
+		JLabel lblSubject = new JLabel("Subject:");
+		lblSubject.setBounds(10, 34, 66, 14);
+		frame.getContentPane().add(lblSubject);
+		
+		textField = new JTextField();
+		textField.setBounds(86, 31, 288, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		btnGenerateTest.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//adding the details to the test
+				System.out.println("adding the test to the db");
+			}
+		});
+		
+
+
+	}
+}
