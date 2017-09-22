08596849cef1d23a97d4e4da18c07639fd4a39b4
Ishay Dahan <ishaydahan@gmail.com>
2017-09-22 20:17:08 +0300

    f

--- src/main/java/com/nlp/common/ApiHolder.java
+++ src/main/java/com/nlp/common/ApiHolder.java
@@ -74,6 +74,8 @@ public class ApiHolder {
 	
 			logger = new PrintStream(new FileOutputStream("logs/analyze_log.txt"));
 			
+//			k
+			
 			langClient = LanguageServiceClient.create();
 			
 			razor[0] = new TextRazor("eb5d5e6284d0ed04a6b7beec96d2805a9bc0f4396cb3fe0c219e5374");//bacoola
